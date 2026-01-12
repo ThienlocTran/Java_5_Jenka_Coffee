9c1fa8face26271f19102a4cb30205d848dfbdd6
ThienlocTran <tranthienloc21102005@gmail.com>
2026-01-12 15:56:29 +0700

    create folder at template and demo page Home

---
+++ src/main/java/com/springboot/jenka_coffee/controller/HomeController.java
@@ -0,0 +1,11 @@
+package com.springboot.jenka_coffee.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class HomeController {
+    @RequestMapping("/home")
+    public String home(){
+        return "index";
+    }
+}
