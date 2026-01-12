dba166f14d61d07a8f9306dbdf30e5b157dbebc1
ThienlocTran <tranthienloc21102005@gmail.com>
2026-01-12 14:46:36 +0700

    Setup base project Spring Boot

---
+++ src/main/java/com/springboot/jenka_coffee/JenkaCoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.jenka_coffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkaCoffeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkaCoffeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/jenka_coffee/JenkaCoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.jenka_coffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkaCoffeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
