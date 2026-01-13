049b214a9128b054c380561728193e0716ecad37
ThienlocTran <tranthienloc21102005@gmail.com>
2026-01-13 14:27:54 +0700

    upload images on cloud and delete folder imegae in source code, don't hard code, smart code

--- src/main/java/com/springboot/jenka_coffee/controller/HomeController.java
+++ src/main/java/com/springboot/jenka_coffee/controller/HomeController.java
@@ -1,11 +1,28 @@
 package com.springboot.jenka_coffee.controller;
 
+import com.springboot.jenka_coffee.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
 public class HomeController {
+
+    final ProductService productService;
+
+    public HomeController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @RequestMapping("/home")
-    public String home(){
+    public String home(org.springframework.ui.Model model) {
+        java.util.List<com.springboot.jenka_coffee.entity.Product> list = productService.findAll();
+        model.addAttribute("items", list);
+
+        // Mocking related products by taking 4 random items (or first 4)
+        // In a real app, you might query by category or tags
+        java.util.Collections.shuffle(list);
+        model.addAttribute("relatedItems", list.stream().limit(4).collect(java.util.stream.Collectors.toList()));
+
         return "index";
     }
 }
